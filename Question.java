9a93be41b78ea1ec68919ddae2f25468b68d54fd
mileadragos <mileadragosstefan@yahoo.com>
2021-08-23 17:18:46 +0300

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Question {
+	String prompt;
+	String answer;
+	
+	public Question(String prompt, String answer){
+		this.prompt = prompt;
+		this.answer = answer;
+	}
+}
